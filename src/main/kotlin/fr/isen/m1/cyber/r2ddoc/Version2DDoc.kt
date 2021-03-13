adf11af049f4c98385fd3898bedd7c8190673822
david haioum <dav6983@gmail.com>
2021-03-13 11:18:05 +0100

    add: temporary parse header v02,

---
+++ src/main/kotlin/fr/isen/m1/cyber/r2ddoc/Version2DDoc.kt
@@ -0,0 +1,20 @@
+package fr.isen.m1.cyber.r2ddoc
+
+enum class Version2DDoc(val headerLength: Int, val version: String) {
+    V01(22, "01"),
+    V02(22, "02");
+
+    companion object {
+        private val supportedVersions: List<Version2DDoc>
+            get() = values().toList()
+
+        fun isSupportedVersion(version: String): Version2DDoc? {
+            supportedVersions.forEach {
+                if (it.version == version) {
+                    return it
+                }
+            }
+            return null
+        }
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,59 @@
+import com.google.zxing.MultiFormatReader
+import com.google.zxing.common.HybridBinarizer
+import com.google.zxing.BinaryBitmap
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource
+import com.google.zxing.LuminanceSource
+import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+import com.google.zxing.NotFoundException
+import com.google.zxing.Result
+import fr.isen.m1.cyber.r2ddoc.Version2DDoc
+import java.io.File
+import java.io.IOException
+
+@Throws(IOException::class, NotFoundException::class)
+fun decodeQRCode(qrCodeImage: String): String? {
+    val file = File(qrCodeImage)
+    val bufferedImage: BufferedImage = ImageIO.read(file)
+    val source: LuminanceSource = BufferedImageLuminanceSource(bufferedImage)
+    val bitmap = BinaryBitmap(HybridBinarizer(source))
+    val result: Result? = MultiFormatReader().decode(bitmap)
+    return result?.text
+}
+
+//TMP function
+fun parseHeader(header: String, version: String) {
+    val versionSupported = Version2DDoc.isSupportedVersion(version)
+    if (header.length == versionSupported?.headerLength) {
+        val identificationMarker: String = header.take(2)
+        val authorityCertificationId: String = header.substring(4, 8)
+        val certificateId: String = header.substring(8, 12)
+        val emissionDocumentDate: String = header.substring(12, 16)
+        val signatureCreationDate: String = header.substring(16, 20)
+        val documentTypeId: String = header.substring(20, 22)
+        println("parsed header: $identificationMarker$version$authorityCertificationId$certificateId$emissionDocumentDate$signatureCreationDate$documentTypeId")
+    } else {
+      println("the header length is not correct")
+    }
+}
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    try {
+        val result = decodeQRCode("./samples/edf.png")
+        result?.substring(2, 4)?.let { version ->
+            Version2DDoc.isSupportedVersion(version)?.let {
+                val header = result.take(it.headerLength)
+                println("message entier: $result")
+                println("version: $version")
+                println("header: $header")
+                parseHeader(header, version)
+                println("reste: ${result.drop(it.headerLength)}")
+                // zone message <US> signature en base32
+            }
+        }
+    } catch (e: IOException) {
+        println(e)
+    } catch (e: NotFoundException) {
+        println("not found: $e")
+    }
 }

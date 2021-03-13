3fe406b53d7bf2e52d354d28010383ede54627d9
david haioum <dav6983@gmail.com>
2021-03-13 12:00:34 +0100

    add: split message's data and signature

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,8 +8,10 @@ import java.awt.image.BufferedImage
 import com.google.zxing.NotFoundException
 import com.google.zxing.Result
 import fr.isen.m1.cyber.r2ddoc.Version2DDoc
+import org.apache.commons.codec.binary.Base32
 import java.io.File
 import java.io.IOException
+import kotlin.math.sign
 
 @Throws(IOException::class, NotFoundException::class)
 fun decodeQRCode(qrCodeImage: String): String? {
@@ -41,14 +43,24 @@ fun main(args: Array<String>) {
     try {
         val result = decodeQRCode("./samples/edf.png")
         result?.substring(2, 4)?.let { version ->
-            Version2DDoc.isSupportedVersion(version)?.let {
-                val header = result.take(it.headerLength)
+            Version2DDoc.isSupportedVersion(version)?.let { version2dDoc ->
+                val header = result.take(version2dDoc.headerLength)
                 println("message entier: $result")
-                println("version: $version")
+                println("version: $version2dDoc")
                 println("header: $header")
                 parseHeader(header, version)
-                println("reste: ${result.drop(it.headerLength)}")
-                // zone message <US> signature en base32
+                val rest = result.drop(version2dDoc.headerLength).split("US")
+                val data = rest[0]
+                println("data: $data")
+                val signature = when(rest.size) {
+                    2 -> rest[1]
+                    else -> ""
+                }
+                if (signature != "") {
+                    val decodedSignature = Base32().decode(signature)
+                    val hexSignature = decodedSignature.joinToString("") { String.format("%02x", it) }
+                    println("hex signature: $hexSignature")
+                }
             }
         }
     } catch (e: IOException) {

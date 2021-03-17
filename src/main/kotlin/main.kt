f4f8e1ba921491282944d2eacf189fa835fb7c64
david haioum <dav6983@gmail.com>
2021-03-17 11:46:41 +0100

    add: splitting algorithm

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,7 +7,9 @@ import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
 import com.google.zxing.NotFoundException
 import com.google.zxing.Result
+import fr.isen.m1.cyber.r2ddoc.Abbreviations
 import fr.isen.m1.cyber.r2ddoc.DataValueIso20022
+import fr.isen.m1.cyber.r2ddoc.Header2DDocC40
 import fr.isen.m1.cyber.r2ddoc.Version2DDoc
 import org.apache.commons.codec.binary.Base32
 import java.io.File
@@ -28,18 +30,57 @@ fun decodeQRCode(qrCodeImage: String): String? {
 fun parseHeader(header: String, version: String) {
     val versionSupported = Version2DDoc.isSupportedVersion(version)
     if (header.length == versionSupported?.headerLength) {
-        val identificationMarker: String = header.take(2)
-        val authorityCertificationId: String = header.substring(4, 8)
-        val certificateId: String = header.substring(8, 12)
-        val emissionDocumentDate: String = header.substring(12, 16)
-        val signatureCreationDate: String = header.substring(16, 20)
-        val documentTypeId: String = header.substring(20, 22)
-        println("parsed header: $identificationMarker$version$authorityCertificationId$certificateId$emissionDocumentDate$signatureCreationDate$documentTypeId")
+        val header2Ddoc = Header2DDocC40(
+            header.take(2),
+            version,
+            header.substring(4, 8),
+            header.substring(8, 12),
+            header.substring(12, 16),
+            header.substring(16, 20),
+            header.substring(20, 22)
+        )
+        println("parsed header: $header2Ddoc")
     } else {
       println("the header length is not correct")
     }
 }
 
+fun parseAbbreviation(value: String): String {
+    return if (value.contains('/')) {
+        var final = ""
+        val split = value.split('/')
+        split.forEach { token ->
+            val abbr = Abbreviations.getAbbreviation(token)
+            final += if (abbr != null) {
+                "${abbr.word} "
+            } else {
+                "$token "
+            }
+        }
+        final
+    } else {
+        value
+    }
+}
+
+fun parseData(data: String): ArrayList<Pair<String, String>> {
+    val parsedData = arrayListOf<Pair<String, String>>()
+    val id = data.substring(0, 2)
+    var next = ""
+    DataValueIso20022.getValue(id)?.let {
+        val value = when {
+            it.maxSize != it.minSize -> data.removePrefix(id).substringBefore(29.toChar()) // ASCII 29 <GS>
+            it.minSize == it.maxSize -> data.removePrefix(id).substring(0, it.maxSize)
+            else -> ""
+        }
+        parsedData.add(Pair(it.label, parseAbbreviation(value)))
+        next = data.removePrefix(id+value).removePrefix(29.toChar().toString())
+    }
+    if (next != "") {
+        parsedData += parseData(next)
+    }
+    return parsedData
+}
 
 fun main(args: Array<String>) {
     try {
@@ -54,27 +95,16 @@ fun main(args: Array<String>) {
                 val rest = result.drop(version2dDoc.headerLength).split(31.toChar()) // ASCII 31 - <US> Unit Separator
                 val data = rest[0]
                 println("data: $data")
-                val dataFields = data.split(29.toChar()) // ASCII 29 - <GS> Group Separator
-                println("fields: $dataFields")
-                dataFields.forEach { field ->
-                    if (field.length >= 2) {
-                        val id = field.substring(0, 2)
-                        var label = ""
-                        DataValueIso20022.getValue(id)?.let { dataValue ->
-                            label = dataValue.label
-                        }
-                        //println("found: ${label.substring(0, dataValue.maxSize)}")
-                        // si label.size > a son max, on resplit en enlevant la partie de ce field en utilisant
-                        var value = field.substring(2, field.length)
-                        if (value == "") {
-                            value = "vide"
-                        }
-                        println("$label $value")
+                val parsedData = parseData(data)
+                println("parsed data:")
+                parsedData.forEach { token ->
+                    val (label, value) = token
+                    if (value == "") {
+                        println("$label: vide")
                     } else {
-                        println("error: $field")
+                        println("$label: $value")
                     }
                 }
-
                 val signature = when(rest.size) {
                     2 -> rest[1]
                     else -> ""
